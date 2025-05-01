793c4ecd3fa813dec3e84dd7d3bab879e08a0d0f
IdevalAlves <ideval-lima@hotmail.com>
2025-05-01 18:01:58 -0300

    Usando metodo Tdd

--- lib/tdd_aula/src/test/java/com/gps/aula/tdd/AppTest.java
+++ lib/tdd_aula/src/test/java/com/gps/aula/tdd/AppTest.java
@@ -1,18 +1,14 @@
 package com.gps.aula.tdd;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-/**
- * Unit test for simple App.
- */
 class AppTest {
-    /**
-     * Rigorous Test.
-     */
+
     @Test
-    void testApp() {
-        assertEquals(1, 1);
+    void testSoma() {
+        Calculadora calc = new Calculadora();
+        int resultado = calc.somar(2, 3);
+        assertEquals(5, resultado);
     }
 }
---
+++ lib/tdd_aula/src/test/java/com/gps/aula/tdd/Calculadora.java
@@ -0,0 +1,7 @@
+package com.gps.aula.tdd;
+
+public class Calculadora {
+    public int somar(int a, int b) {
+        return a + b;
+    }
+}
